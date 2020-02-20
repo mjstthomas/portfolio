a26bf66b8a150c2ff9165d24134289c8f4fc86ee
Mathew <mjstthomas0516@gmail.com>
2020-02-20 05:18:57 -0700

    removed all JS

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
 console.log('connected')
-
-function bio(){
-	$('.bio').on("click", event=>{
-		$('.bio').addClass('selected')
-		$('.projects').removeClass('selected')
-	})
-};
-function projects(){
-	$('.projects').on('click', event=>{
-		$('.projects').addClass('selected');
-		$('.bio').removeClass('selected');
-		})
-};
-bio();
-projects();
